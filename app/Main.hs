cacd6e9aae60e24ebb28e3f918e08f3070bd1799
Daniel Slapman <danslapman@gmail.com>
2018-12-23 01:58:48 +0300

    Use `concatUnion` in root case

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,8 @@ import Data.Aeson
 import Data.Function
 import Json2Csv
 
-sut = decode "{\"foo\": [\"abc\",\"def\", {\"peka\": 1}]}" :: Maybe Value
+--sut = decode "{\"foo\": [\"abc\",\"def\", {\"peka\": 1}]}" :: Maybe Value
+sut = decode "[{\"peka\": 1}, {\"peka\": 2}, {\"peka\": 3}]" :: Maybe Value
 
 main :: IO ()
 main = print $ fmap (computeHeader True) sut
--- src/Json2Csv.hs
+++ src/Json2Csv.hs
@@ -2,11 +2,14 @@
 
 module Json2Csv (computeHeader) where
 
+import Control.Lens ((^?))
 import Data.Aeson
+import Data.Aeson.Lens
 import Data.Function
+import Data.List (union)
 import Data.List.Index
 import qualified Data.HashMap.Strict as HM
-import Data.Text hiding (map, concat, concatMap)
+import Data.Text (Text)
 import Data.Text.Buildable
 import Data.Text.Format
 import Data.Text.Lazy (toStrict)
@@ -17,6 +20,9 @@ appendPrefix :: Buildable b => Text -> [b] -> [Text]
 appendPrefix prefix [] = [prefix]
 appendPrefix prefix names = map (\name -> format "{}.{}" (prefix, name) & toStrict) names
 
+concatUnion :: Eq a => [[a]] -> [a]
+concatUnion = foldl1 union
+
 computeHeader :: Bool -> Value -> [Text]
 computeHeader _ Null = []
 computeHeader _ (Bool _) = []
@@ -24,6 +30,6 @@ computeHeader _ (Number _) = []
 computeHeader _ (String _) = []
 computeHeader False (Array arr) =
   concat . (imap (\idx -> appendPrefix $ showt idx)) . (fmap (computeHeader False)) . toList $ arr
-computeHeader True (Array arr) = concat . (fmap (computeHeader False)) . toList $ arr
+computeHeader True (Array arr) = concatUnion . (fmap (computeHeader False)) . toList $ arr
 computeHeader _ (Object obj) =
   concatMap (uncurry appendPrefix) . HM.toList . (HM.map (computeHeader False)) $ obj
