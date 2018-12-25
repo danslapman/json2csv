85723bc1185cda12a4b72ce66699643830eca2a3
Daniel Slapman <danslapman@gmail.com>
2018-12-25 17:36:49 +0300

    Add VecPats

---
+++ src/VecPats.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module VecPats (pattern (:::)) where
+
+import Data.Maybe
+import Data.Vector
+
+uncons :: Vector a -> Maybe (a, Vector a)
+uncons v = if null v
+  then Nothing
+  else Just (unsafeHead v, unsafeTail v)
+
+pattern x ::: xs <- (uncons -> Just (x, xs))
