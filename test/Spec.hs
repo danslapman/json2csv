ebf7784d3b9a465bd6892c3638f88777bd072018
Daniel Slapman <danslapman@gmail.com>
2025-04-30 15:38:57 +0200

    Simplify test run

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,17 +1,9 @@
 import HashSetUtilsSpec
 import SequenceUtilsSpec
 import Test.HUnit
-import System.Exit (exitFailure)
-
-processCounts :: Counts -> IO ()
-processCounts counts = do
-  if errors counts + failures counts == 0
-  then return ()
-  else exitFailure
 
 main :: IO ()
 main = do
-  hsCounts <- runTestTT hashSetTestCases
-  processCounts hsCounts
-  seqCounts <- runTestTT seqTestCases
-  processCounts seqCounts
+  _ <- runTestTT hashSetTestCases
+  _ <- runTestTT seqTestCases
+  return ()
